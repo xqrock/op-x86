442ab13d921d8ed5593c90e517b67da592c1f182
xqrock <58764635+xqrock@users.noreply.github.com>
2024-01-31 14:21:32 +0800

    Update lede-part1.sh

--- lede-part1.sh
+++ lede-part1.sh
@@ -9,6 +9,19 @@
 #=============================================================
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=6.2/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    mv $2 package/custom/
+    rm -rf $repo
+}
+function drop_package(){
+    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
+}
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
@@ -58,3 +71,11 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-weburl package/luci
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-timecontrol package/luci-app-timecontrol
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-timewol package/luci-app-timewol
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
+merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
+merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/adguardhome
+merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-adguardhome
+merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-timewol
+merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-timecontrol
+merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-weburl
+merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-webrestriction
+
