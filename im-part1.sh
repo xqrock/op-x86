ab975098341c333f846ae5f9da98477cdad5f3bc
xqrock <58764635+xqrock@users.noreply.github.com>
2024-03-07 09:34:55 +0800

    Update im-part1.sh

--- im-part1.sh
+++ im-part1.sh
@@ -61,3 +61,4 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/adguardhome
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-adguardhome
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/wrtbwmon
+merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-wrtbwmon
