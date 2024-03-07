4b8f75a387fc62dafe10bf1ebd9850585a21bc33
xqrock <58764635+xqrock@users.noreply.github.com>
2024-03-07 10:46:41 +0800

    Create lede61-part2.sh

---
+++ lede61-part2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+rm -rf ./feeds/luci/applications/luci-app-wrtbwmon
+rm -rf ./feeds/luci/applications/luci-app-mosdns
+rm -rf ./feeds/packages/net/mosdns
