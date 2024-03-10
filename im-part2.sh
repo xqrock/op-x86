3b5fcbbbc577498afef1bd4b0c8ae90cd33eb7a5
xqrock <58764635+xqrock@users.noreply.github.com>
2024-03-10 21:56:08 +0800

    Update im-part2.sh

--- im-part2.sh
+++ im-part2.sh
@@ -21,6 +21,6 @@ rm -rf ./feeds/packages/net/mosdns
 rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/luci/applications/luci-app-ssr-plus
 rm -rf ./feeds/luci/applications/luci-app-openclash
-# rm -rf ./feeds/luci/applications/luci-app-smartdns
+rm -rf ./feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
