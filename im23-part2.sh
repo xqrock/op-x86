de8eaabd4fa58abaaa2077b152957f7533dd9b06
xqrock <58764635+xqrock@users.noreply.github.com>
2024-03-15 20:28:03 +0800

    Update im23-part2.sh

--- im23-part2.sh
+++ im23-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.188.253/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.188.253/g' package/base-files/files/bin/config_generate
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 # rm -rf ./feeds/luci/luci-app-passwall
 rm -rf ./feeds/packages/net/adguardhome
