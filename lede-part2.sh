53da722f61819420b63e4b75c9160987a299fa89
xqrock <58764635+xqrock@users.noreply.github.com>
2024-03-15 20:28:19 +0800

    Update lede-part2.sh

--- lede-part2.sh
+++ lede-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.188.251/g' package/base-files/files/bin/config_generate
 # rm -rf ./feeds/luci/themes/luci-theme-argon
 # rm -rf ./feeds/luci/applications/luci-app-wrtbwmon
 # rm -rf ./feeds/packages/net/adguardhome
