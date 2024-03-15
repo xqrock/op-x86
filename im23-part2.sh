05144707ee7cb728e267b1039bd7ec8c43db70b4
xqrock <58764635+xqrock@users.noreply.github.com>
2024-03-15 20:33:26 +0800

    Update im23-part2.sh

--- im23-part2.sh
+++ im23-part2.sh
@@ -23,5 +23,6 @@ rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/luci/applications/luci-app-ssr-plus
 rm -rf ./feeds/luci/applications/luci-app-openclash
 rm -rf ./feeds/luci/applications/luci-app-smartdns
+rm -rf ./feeds/luci/applications/luci-app-wechatpush
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
