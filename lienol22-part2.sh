dc627533221e6d20ceee10c9cc6c9ddfed483812
xqrock <58764635+xqrock@users.noreply.github.com>
2024-03-16 22:57:05 +0800

    Update lienol22-part2.sh

--- lienol22-part2.sh
+++ lienol22-part2.sh
@@ -12,4 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-rm -rf ./feeds/other/luci-app-adguardhome
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
